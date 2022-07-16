bd75155f3c449aa49ff5a70fffc08e5fd3f8643e
Ubuntu <ubuntu@ip-172-31-21-225.us-west-1.compute.internal>
2022-07-16 16:51:46 +0000

    ユーザーコントローラーを追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new', to: 'users#new'
   root 'application#hello'
   get    '/task_create',   to: 'tasks#new'
   post   '/task_create', to: 'tasks#create'
