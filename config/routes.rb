dd1c78a66fd754603704728c536745950322ca78
Ubuntu <ubuntu@ip-172-31-71-164.ec2.internal>
2022-07-21 09:04:35 +0000

    ポストルーティング　/task/:id/toggle　task→tasks に変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'task_lists/new'
   root 'application#hello'
   get 'tasks/show'
-  post '/task/:id/toggle', to: 'tasks#toggle'
+  post '/tasks/:id/toggle', to: 'tasks#toggle'
   post '/task_lists/:id',  to: 'tasks#create'
   resources :tasks
   resources :task_lists
